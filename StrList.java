e3a05c5fdb6bec927cc9c6e653e7070b34f643b2
Will Huiras <whuiras@pdx.edu>
2017-05-22 11:59:37 -0700

    Added StrList class

---
+++ StrList.java
@@ -0,0 +1,18 @@
+public class StrList {
+
+    import java.util.List;
+    import java.util.ArrayList;
+
+
+    public static List<String> makeList(String str1, String str2, String str3) {
+	List<String> myList = new ArrayList<String>();
+	myList.add(str1);
+	myList.add(str2);
+	myList.add(str3);
+	return myList;
+    }
+}
+
+    
+
+    
