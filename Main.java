ae9aef78165a9fa433dfaa10918b8f68e7f68777
mmcbee1 <meghankmcbee@gmail.com>
2017-05-21 19:39:48 -0700

    Added lex comparison function and basic test function in Main

--- Main.java
+++ Main.java
@@ -2,5 +2,18 @@ public class Main{
 	public static void main(String args[])
 	{
 		System.out.println("running demo");
-	}
+	
+		testStrCompare();
+
+        }
+
+        private static void testStrCompare() 
+        {
+            String str1 = "banana";
+            String str2 = "apples";
+
+            System.out.println(str1 + " > " + str2 + ", should return 1: " + StrCompare.lexCompare(str1,str2));
+            System.out.println(str2 + " < " + str1 + ", should return -1: " + StrCompare.lexCompare(str2,str1));
+            System.out.println(str1 + " = " + str1 + ", should return 0: " + StrCompare.lexCompare(str1,str1));
+        }
 }
--- StrCompare.java
+++ StrCompare.java
@@ -1,11 +1,23 @@
+/*
+*  Created by Meghan McBee
+*  Last Update: May 21, 2017
+*/
 
 public class StrCompare {
 
+
+    // Compares two strings lexographically
+    public static int lexCompare(String str1, String str2)
+    {
+        return str1.compareTo(str2);
+    }
+
+
     // Compares length of two given strings
-    public static int lexCompare(String string1, String string2)
+    public static int strCompare(String str1, String str2)
     {    
-        int strlen1 = string1.length();
-        int strlen2 = string2.length();
+        int strlen1 = str1.length();
+        int strlen2 = str2.length();
 
         if(strlen1 < strlen2){
             return -1;
